b7c516491f4eccac509bef3054f9afe42f02cb9b
Gianni Bortolo Bossini <gianni.bossini@codiceplastico.com>
2023-09-03 22:12:28 +0200

    Aggiunti log su file e metriche su ZipKin

--- Orleans.UrlShortner/Grains/UrlShortnerStatisticsGrain.cs
+++ Orleans.UrlShortner/Grains/UrlShortnerStatisticsGrain.cs
@@ -12,15 +12,26 @@ public interface IUrlShortnerStatisticsGrain : IGrainWithStringKey
 
 public class UrlShortnerStatisticsGrain : Grain, IUrlShortnerStatisticsGrain
 {
+    private readonly ILogger<UrlShortnerStatisticsGrain> logger;
+
+    public UrlShortnerStatisticsGrain(ILogger<UrlShortnerStatisticsGrain> logger)
+    {
+        this.logger = logger;
+    }
+
     public int TotalActivations { get; set; }
 
     public Task<int> GetTotal()
     {
+        logger.LogInformation("Total activations: {TotalActivations}.", TotalActivations);
+
         return Task.FromResult(TotalActivations);
     }
 
     public Task RegisterNew()
     {
+        logger.LogInformation($"New activation registered!.");
+
         this.TotalActivations++;
         return Task.CompletedTask;
     }
